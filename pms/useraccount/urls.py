20ac481edadb7a7fb00c772555cd0c0012128508
zsw <zsw18049252607@163.com>
2018-03-12 16:32:35 +0800

    登录系统

--- pms/useraccount/urls.py
+++ pms/useraccount/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url('^login/',views.login,name='name'),
+    url('^login/', views.login, name='login'),
+    url('^logout/', views.logout, name='logout'),
 ]
--- pms/useraccount/views.py
+++ pms/useraccount/views.py
@@ -1,8 +1,9 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.contrib import auth
 from .models import User
 # Create your views here.
 
@@ -16,4 +17,22 @@ class StudentBackend(ModelBackend):
             return None
 
 def login(request):
-    return HttpResponse(u'登录成功')
+
+    if request.user.is_authenticated(): 
+        return HttpResponseRedirect('/index/')
+    
+    if request.method == 'GET':
+        return render(request,'useraccount/login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect('/index/')
+        else:
+            return render(request,'useraccount/login.html',{'error_message':'账号或密码错误，请重新登录！'}) 
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/')
