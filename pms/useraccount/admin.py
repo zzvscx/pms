61ef36959041dec96a22c0c1ae05d4d765b3c9cc
zsw <zsw18049252607@163.com>
2018-03-13 14:42:08 +0800

    user_admin

--- pms/useraccount/admin.py
+++ pms/useraccount/admin.py
@@ -1,19 +1,91 @@
 #-*- coding: utf-8 -*-
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-from .models import User
+from .models import User, Academy, Department, Grade, Course, UserScore
+from django import forms
+from django.contrib.auth.forms import UserChangeForm
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation', widget=forms.PasswordInput)
+    
+
+    class Meta:
+        model = User
+        fields = ('username', 'email',)
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(UserChangeForm):
+    username = forms.CharField(max_length=36)
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def clean_username(self):
+        return self.cleaned_data.get('username')
+
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
+
+    form = UserChangeForm
+    add_form = UserCreationForm
     fieldsets = [
-        (None, {'fields': ('stuId','username','password')}),
-        (u'权限', {'fields': ('is_active','is_staff','is_superuser',
-                        'groups','user_permissions')}),
-        (u'重要日期',{'fields': ('last_login','date_joined')}),
+        (None, {'fields': ('stuId', 'username', 'password', 'grade', 'intake')}),
+        (u'权限', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                            'groups', 'user_permissions')}),
+        (u'重要日期', {'fields': ('last_login', 'date_joined')}),
     ]
 
-    list_display = ('stuId','username','is_active','is_staff')
-    search_fields = ('stuId','username')
-    list_filter = ('is_staff','is_active')
+    list_display = ('stuId', 'username', 'grade',
+                    'intake', 'is_active', 'is_staff')
+    search_fields = ('grade', 'stuId', 'username')
+    list_filter = ('is_staff', 'is_active')
+    raw_id_fields = ('grade',)
+
+
+@admin.register(Academy)
+class AcademyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'admin')
+    search_fields = ('name', 'admin')
+    raw_id_fields = ('admin',)
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'admin', 'academy')
+    search_fields = ('name', 'admin', 'academy')
+    raw_id_fields = ('admin', 'academy')
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department')
+    search_fields = ('name', 'department')
+    raw_id_fields = ('department',)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'total_points', 'rate')
+    search_fields = ('name',)
+    raw_id_fields = ('admin',)
--- pms/useraccount/models.py
+++ pms/useraccount/models.py
@@ -13,21 +13,19 @@ AbstractUser._meta.get_field('username').max_length = 36
 
 
 class User(AbstractUser):
+    name = models.CharField(default='',max_length=32,verbose_name=u'姓名') 
     stuId = models.CharField(max_length=32, verbose_name=u'学号/教职工编号')
-    grade = models.CharField(max_length=32, verbose_name=u'班级')
+    grade = models.ForeignKey('Grade',null=True,blank=True,max_length=32, verbose_name=u'班级')
     intake = models.DateField(verbose_name=u'入学时间')
-    academy = models.ForeignKey(Academy, verbose_name=u'学院')
-    department = models.ForeignKey(Department, verbose_name=u'系')
 
     def __unicode__(self):
-        return self.username
+        return self.name
 
     def __str__(self):
-        return self.username
-
+        return self.name
 
 class Academy(models.Model):
-    name = models.CharField(max_length=64, verbose_name=u'学院名')
+    name = models.CharField(max_length=64, verbose_name=u'学院')
     admin = models.ForeignKey(User, null=True, blank=True, verbose_name=u'院长')
 
     def __unicode__(self):
@@ -38,14 +36,19 @@ class Academy(models.Model):
 
 
 class Department(models.Model):
-    name = models.CharField(max_length=64, verbose_name=u'系名')
-    admin = models.ForeignKey(User, null=True, blanl=True, verbose_name=u'系主任')
+    academy = models.ForeignKey(Academy,verbose_name=u'学院')
+    name = models.CharField(max_length=64, verbose_name=u'系')
+    admin = models.ForeignKey(User, null=True, blank=True, verbose_name=u'系主任')
 
     def __unicode__(self):
         return self.name, self.admin
 
     def __str__(self):
         return self.name, self.admin
+
+class Grade(models.Model):
+    name = models.CharField(max_length=32,verbose_name=u'班级')
+    department = models.ForeignKey(Department,verbose_name='系')
 
 
 class Course(models.Model):
