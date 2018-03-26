9f085231c6e73b94910bc402b049656bce75ebec
zsw <zsw18049252607@163.com>
2018-03-26 21:01:27 +0800

    下载成绩

--- pms/useraccount/views.py
+++ pms/useraccount/views.py
@@ -69,13 +69,13 @@ def user_points(request):
                            scores['count'], scores['sum_points'], scores['avg_points']])
     all_score = userscores.aggregate(
         sum_points=Sum('points'), avg_points=Avg('points'))
-    all_score = [str(userscores.count()), str(all_score['sum_points']),
-                 str(all_score['avg_points'])]
+    all_score = [userscores.count(), all_score['sum_points'],
+                 all_score['avg_points']]
     if request.GET.get('download', False):
         data = [[u'学年学期', u'门数', u'总学分', u'平均绩点'], ]
         data.extend(term_score)
         all_score.insert(0,u'在校汇总')
-        data.extend(all_score)
+        data.append(all_score)
         all_data = [[u'学年学期', u'课程代码', u'课程编号', u'课程名称', u'课程类别', u'学分',
                      u'期中成绩', u'期末成绩', u'平时成绩', u'补考成绩', u'总评成绩', u'实验成绩', u'最终成绩', u'绩点'], ]
         for userscore in userscores:
@@ -84,22 +84,11 @@ def user_points(request):
                              course.numbering, course.name, course.category.name, course.credit, userscore.midterm,
                              userscore.final_exam, userscore.usual, userscore.retest, userscore.total_score,
                              userscore.experimental, userscore.total_score, userscore.points])
-        name = u'{}{}成绩清单'.format(datetime.now().strftime(
+        name = u'{}{}成绩清单.xls'.format(datetime.now().strftime(
             '%Y%m%d'), request.user.name or request.user.username)
-        excel = generate_xls_multisheet([{'name': u'成绩列表', 'data': data}, {
-                                        'name': u'成绩汇总', 'data': all_data}])
+        excel = generate_xls_multisheet([{'name': u'成绩列表', 'data': data}, {'name': u'成绩汇总', 'data': all_data}])
         response = HttpResponse(excel, content_type='application/vnd.ms-excel')
-        response['Content-Disposition'] = u'attachment; filename=%s' % urllib.quote(
-            name.encode('utf8'))
+        response['Content-Disposition'] = u'attachment; filename=%s' % urllib.quote(name.encode('utf8'))
         return response
     return render(request, 'useraccount/achievement.html', locals())
 
-
-@login_required
-def download_points_excel(request):
-    name = u'{}{}成绩清单'.format()
-    excel = generate_xls_multisheet(
-        [{'name': u'成绩', 'data': data}, {'name': u'汇总', 'data': all_data}])
-    response = HttpResponse(excel, content_type='application/vnd.ms-excel')
-    response['Content-Disposition'] = u'attachment; filename=%s' % urllib.quote(
-        name.encode('utf8'))
