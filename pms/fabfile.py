c292543862c13f02ca24ad69c4b7ef5242f7c82c
zsw <zsw18049252607@163.com>
2018-04-18 22:47:22 +0800

    fix

--- pms/fabfile.py
+++ pms/fabfile.py
@@ -9,12 +9,11 @@ env.port = '22'
 
 def deploy():
     source_folder = '/home/ubuntu/pms/pms'
-    run('cd {} && git pull origin master '.format(source_folder))
+    run('cd {} && git checkout -f && git pull origin master '.format(source_folder))
     run(
         '''
         cd {} &&
         ~/venv/venv_pms/bin/pip install -r ../requirements.txt &&
-        ~/venv/venv_pms/bin/python manage.py collectstatic --noinput &&
         ~/venv/venv_pms/bin/python manage.py migrate
         '''.format(source_folder))
     sudo('restart pms')
