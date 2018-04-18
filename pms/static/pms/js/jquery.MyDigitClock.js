349b9e328125094da535709cfe224126eed691fa
zsw <zsw18049252607@163.com>
2018-04-18 22:40:25 +0800

    修改css

---
+++ pms/static/pms/js/jquery.MyDigitClock.js
@@ -0,0 +1,80 @@
+/**
+ *
+ * Version 0.5
+ *
+ */
+	
+(function($) {
+
+    var _options = {};
+	var _container = {};
+
+	jQuery.fn.MyDigitClock = function(options) {
+		var id = $(this).get(0).id;
+		_options[id] = $.extend({}, $.fn.MyDigitClock.defaults, options);
+
+		return this.each(function()
+		{
+			_container[id] = $(this);
+			showClock(id);
+		});
+		
+		function showClock(id)
+		{
+			var d = new Date;
+			var h = d.getHours();
+			var m = d.getMinutes();
+			var s = d.getSeconds();
+			var ampm = "";
+			if (_options[id].bAmPm)
+			{
+				if (h>12)
+				{
+					h = h-12;
+					ampm = " PM";
+				}
+				else
+				{
+					ampm = " AM";
+				}
+			}
+			
+			var templateStr = _options[id].timeFormat + ampm;
+			templateStr = templateStr.replace("{HH}", getDD(h));
+			templateStr = templateStr.replace("{MM}", getDD(m));
+			templateStr = templateStr.replace("{SS}", getDD(s));
+		
+			var obj = $("#"+id);
+			obj.css("fontSize", _options[id].fontSize);
+			obj.css("fontFamily", _options[id].fontFamily)
+			obj.css("background", _options[id].background);
+			obj.css("fontWeight", _options[id].fontWeight);
+			obj.css("opacity",1)
+			//change reading
+			obj.html(templateStr)
+			
+			//toggle hands
+			if (_options[id].bShowHeartBeat)
+			{
+				obj.find("#ch1").fadeTo(800, 0.1);
+				obj.find("#ch2").fadeTo(800, 0.1);
+			}
+			setTimeout(function(){showClock(id)}, 1000);
+		}
+		
+		function getDD(num)
+		{
+			return (num>=10)?num:"0"+num;
+		}
+		
+		function refreshClock()
+		{
+			setupClock();
+		}
+	}
+	
+	//default values
+	jQuery.fn.MyDigitClock.defaults = {
+	};
+
+})(jQuery);
