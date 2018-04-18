7b3053d85ce90f789cffdbc2fee6028fb7bf9159
zsw <zsw18049252607@163.com>
2018-04-18 11:45:09 +0800

    fabric

---
+++ pms/fabfile.py
@@ -0,0 +1,21 @@
+from fabric.api import env, run
+from fabric.operations import sudo
+
+GIT_REPO = "pms"
+
+env.user = 'ubuntu'
+env.hosts = ['118.25.44.135']
+env.port = '22'
+
+def deploy():
+    source_folder = '/home/ubuntu/pms/pms'
+    run('cd {} && git pull origin master '.format(source_folder))
+    run(
+        '''
+        cd {} &&
+        ~/venv/venv_pms/bin/pip install -r ../requirements.txt &&
+        ~/venv/venv_pms/bin/python manage.py collectstatic --noinput &&
+        ~/venv/venv_pms/bin/python manage.py migrate
+        '''.format(source_folder))
+    sudo('restart pms')
+    sudo('service nginx reload')
