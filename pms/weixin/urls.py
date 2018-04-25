9cc719c3762ff41ea98bf31d3525407d5edbd780
zsw <zsw18049252607@163.com>
2018-04-25 12:28:07 +0800

    fix

--- pms/weixin/urls.py
+++ pms/weixin/urls.py
@@ -4,7 +4,7 @@
 from django.conf.urls import patterns, include, url
 
 from weixin.views import *
-from weixin.weixinbase import handleRequest,
+from weixin.weixinbase import handleRequest
 
 urlpatterns = patterns(\
     'weixin.views',
